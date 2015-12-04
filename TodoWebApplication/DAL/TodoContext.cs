f20f79c4667af3f9c4c3a2d2295b4fa48a435c20
Blair Harris <jonathan.blair.harris@gmail.com>
2015-12-04 14:39:10 +0000

    quick update

--- TodoWebApplication/DAL/TodoContext.cs
+++ TodoWebApplication/DAL/TodoContext.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using TodoWebApplication.Models;
 
 namespace TodoWebApplication.DAL
 {
     public class TodoContext : DbContext
     {
+        public TodoContext() : base("TodoContext")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
---
+++ TodoWebApplication/DAL/TodoInitialiser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using TodoWebApplication.Models;
+
+namespace TodoWebApplication.DAL
+{
+    public class TodoInitialiser : System.Data.Entity.DropCreateDatabaseIfModelChanges<TodoContext>
+    {
+        protected override void Seed(TodoContext context)
+        {
+            var users = new List<User>
+            {
+                new User { Name = "Blair" },
+                new User { Name = "Vicky" }
+            };
+
+            foreach( User user in users)
+            {
+                context.Users.Add(user);
+            }
+            users.ForEach(u => context.Users.Add(u));
+            context.SaveChanges();
+        }
+    }
+}
