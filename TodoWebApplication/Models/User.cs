a4e386606b59623be2665e01ffb05364a8d53ccf
Blair Harris <jonathan.blair.harris@gmail.com>
2015-12-04 17:16:53 +0000

    Finished for the day

--- TodoWebApplication/Models/User.cs
+++ TodoWebApplication/Models/User.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace TodoWebApplication.Models
 {
@@ -9,6 +11,8 @@ namespace TodoWebApplication.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage ="Invalid email adress numpty")]
         public string Email { get; set; }
     }
 }
